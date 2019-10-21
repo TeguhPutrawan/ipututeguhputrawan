d572632e809e255eab4e3eb9b0747ef3cebe6a16
TeguhPutrawan <ipututeguhputrawan@gmail.com>
2019-10-21 21:21:36 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+$('.page-scroll').on('click', function(e){
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+      
+            // Store hash
+            var hash = this.hash;
+      
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html').animate({
+              scrollTop: $(hash).offset().top }, 1250, function(){
+              // Add hash (#) to URL when done scrolling (default click behavior)
+              window.location.hash = hash;
+            });
+        }
+   
+});
+});
